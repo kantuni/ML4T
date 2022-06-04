329232a9b472c1975cd2411fdcb9cd387185020c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-06-03 22:26:18 -0400

    Prettify

--- src/course/lesson_1.py
+++ src/course/lesson_1.py
@@ -5,18 +5,21 @@ import pandas as pd
 
 
 def last_five_rows(symbol: str) -> None:
+    """Print last 5 rows of the given symbol"""
     file_path = Path("data/{}.csv".format(symbol))
     df = pd.read_csv(file_path)
     print(df.tail(5))
 
 
 def get_mean_volume(symbol: str) -> float:
+    """Print mean volume of the given symbol"""
     file_path = Path("data/{}.csv".format(symbol))
     df = pd.read_csv(file_path)
     return df["Volume"].mean()
 
 
 def plot_high_prices(symbol: str) -> None:
+    """Plot high prices of the given symbol"""
     file_path = Path("data/{}.csv".format(symbol))
     df = pd.read_csv(file_path)
     df["High"].plot(title="High Prices")
@@ -26,12 +29,12 @@ def plot_high_prices(symbol: str) -> None:
 
 
 if __name__ == "__main__":
-    # Print last 5 rows of the given symbol data
+    # Print last 5 rows of AAPL
     # last_five_rows("AAPL")
 
-    # Print mean volume of the given symbol data
+    # Print mean volume of AAPL, MSFT, and GOOG
     # for symbol in ["AAPL", "MSFT", "GOOG"]:
     #     print(symbol, get_mean_volume(symbol))
 
-    # Plot High prices for IBM
+    # Plot high prices of IBM
     plot_high_prices("IBM")
