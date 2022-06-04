d9b13ac7fe7f7c87eb31b9c9af17396da5799acb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-06-03 22:21:34 -0400

    Lesson 1: Part 13

--- src/course/lesson_1.py
+++ src/course/lesson_1.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -15,10 +16,22 @@ def get_mean_volume(symbol: str) -> float:
     return df["Volume"].mean()
 
 
+def plot_high_prices(symbol: str) -> None:
+    file_path = Path("data/{}.csv".format(symbol))
+    df = pd.read_csv(file_path)
+    df["High"].plot(title="High Prices")
+    plt.xlabel("Time")
+    plt.ylabel("Price")
+    plt.show()
+
+
 if __name__ == "__main__":
     # Print last 5 rows of the given symbol data
     # last_five_rows("AAPL")
 
     # Print mean volume of the given symbol data
-    for symbol in ["AAPL", "MSFT", "GOOG"]:
-        print(symbol, get_mean_volume(symbol))
+    # for symbol in ["AAPL", "MSFT", "GOOG"]:
+    #     print(symbol, get_mean_volume(symbol))
+
+    # Plot High prices for IBM
+    plot_high_prices("IBM")
