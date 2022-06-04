441e146d1be6dadd32da95e304ba3f4f8267a7de
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-06-04 18:02:44 -0400

    Lesson 3: Part 9

---
+++ src/course/lesson_3.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+if __name__ == "__main__":
+    # Create a numpy array from Python list.
+    # print(np.array([1, 2, 3]))
+    # print(np.array([[1, 2, 3], [4, 5, 6]]))
+
+    # Create an empty numpy array.
+    # The values will be whatever is in the memory at that location.
+    # print(np.empty(5))
+    # print(np.empty((2, 3)))
+
+    # A numpy array of 1s.
+    # print(np.ones(5))
+
+    # A numpy array of 0s.
+    # print(np.zeros((2, 3)))
+
+    # A numpy array of 1s as ints.
+    print(np.ones(5, dtype=int))
+
+    # A numpy array of random numbers, uniformly sampled from [0, 1).
+    print(np.random.random((2, 3)))
+    # Same as above, just different syntax.
+    print(np.random.rand(2, 3))
+
+    # A numpy array of random numbers, sampled from normal (Gaussian) distribution.
+    # mean = 0, standard deviation = 1.
+    print(np.random.normal(size=(2, 3)))
+    # mean = 100, standard deviation = 10.
+    print(np.random.normal(100, 10, size=(2, 3)))
+
+    # A numpy array of random integers, uniformly sampled from [0, 10).
+    print(np.random.randint(0, 10, size=(2, 3)))
