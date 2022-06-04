6a5769c2ffe788db6521759de5d9263afaef91ff
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-06-03 22:16:45 -0400

    Lesson 1: Part 11

--- src/course/lesson_1.py
+++ src/course/lesson_1.py
@@ -1,5 +1,3 @@
-"""Print last 5 rows of the given symbol data"""
-
 from pathlib import Path
 
 import pandas as pd
@@ -7,10 +5,20 @@ import pandas as pd
 
 def last_five_rows(symbol: str) -> None:
     file_path = Path("data/{}.csv".format(symbol))
-    print("file_path", file_path)
     df = pd.read_csv(file_path)
     print(df.tail(5))
 
 
+def get_mean_volume(symbol: str) -> float:
+    file_path = Path("data/{}.csv".format(symbol))
+    df = pd.read_csv(file_path)
+    return df["Volume"].mean()
+
+
 if __name__ == "__main__":
-    last_five_rows("AAPL")
+    # Print last 5 rows of the given symbol data
+    # last_five_rows("AAPL")
+
+    # Print mean volume of the given symbol data
+    for symbol in ["AAPL", "MSFT", "GOOG"]:
+        print(symbol, get_mean_volume(symbol))
